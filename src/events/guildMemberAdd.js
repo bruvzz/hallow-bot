53f7fc3a17606b3822b581d1fc0324b5104b2709
Ducky <97487136+bruvzz@users.noreply.github.com>
2025-10-24 23:18:14 -0400

    Refactor invite handling in guildMemberAdd event

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,4 +1,6 @@
-const { addInvite } = require("../commands/candyHelper");
+const { 
+  addInvite, 
+} = require("../commands/candyHelper");
 
 module.exports = {
   name: "guildMemberAdd",
@@ -9,24 +11,31 @@ module.exports = {
    */
   callback: async (client, member) => {
     try {
-      const cachedInvites = client.invitesCache.get(member.guild.id);
+      const cachedInvites = client.invitesCache.get(member.guild.id) || new Map();
       const newInvites = await member.guild.invites.fetch();
+
       const usedInvite = newInvites.find(
-        i => cachedInvites.get(i.code)?.uses < i.uses
+        i => (cachedInvites.get(i.code)?.uses || 0) < i.uses
       );
 
       if (!usedInvite || !usedInvite.inviter) return;
 
       const inviterId = usedInvite.inviter.id;
-      const candyReward = addInvite(inviterId);
-      if (candyReward > 0) {
-        const inviterUser = await client.users.fetch(inviterId);
-        inviterUser.send(
-          `🎃 Congrats! You earned **${candyReward} 🍬 candy points** for 5 successful invites!`
-        );
+
+      const bonusCandy = addInvite(inviterId);
+
+      if (bonusCandy > 0) {
+        try {
+          const inviterUser = await client.users.fetch(inviterId);
+          inviterUser.send(
+            `🎃 Congrats! You earned **${bonusCandy} 🍬 candy points** for reaching 5 successful invites!`
+          );
+        } catch (err) {
+          console.error(`Failed to DM inviter ${inviterId}:`, err);
+        }
       }
 
-      client.invitesCache.set(member.guild.id, newInvites);
+      client.invitesCache.set(member.guild.id, new Map(newInvites.map(i => [i.code, i])));
     } catch (err) {
       console.error("Error in guildMemberAdd event:", err);
     }
